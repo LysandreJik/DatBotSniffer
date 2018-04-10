011ba4f1b7c243a30500e5a1983b95ee47cd55d5
Lysandre <lysandre.debut@reseau.eseo.fr>
2018-04-10 20:48:18 +0200

    Initialized the packet sniffer

---
+++ src/fr/main/sniffer/Main.java
@@ -0,0 +1,10 @@
+package fr.main.sniffer;
+
+/**
+ * Main class for the datbot packet sniffer
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Initializing the packet sniffer");
+    }
+}
